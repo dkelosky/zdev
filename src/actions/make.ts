dbe3aab7a4c72dbd496879443c2183d93aa0f44b
Dan Kelosky <dkelosky@gmail.com>
2021-06-04 10:52:39 -0400

    exit 1
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- src/actions/make.ts
+++ src/actions/make.ts
@@ -8,11 +8,24 @@ export async function make(target: string, getListing = true) {
     const makeCmd = `${ZOWE} uss issue ssh \\"cd ${dir} && make ${target}\\"`;
     const strResp = await runCmd(makeCmd);
 
+    const errRegex = new RegExp(/.*ERROR.*/g);
+    const warnRegex = new RegExp(/.*WARNING.*/g);
+
     if (strResp) {
         console.log(`...${strResp}`);
         if (getListing) {
             const listings = await getListings(strResp);
             await downloadListingFiles(listings);
+        }
+
+        if (errRegex.test(strResp))
+        {
+            console.log(`❌ stopping for error condition`)
+            process.exit(1);
+        }
+        if (warnRegex.test(strResp)) {
+            console.log(`⚠️ stopping for warning condition`)
+            process.exit(1);
         }
     } else {
         console.log(`⚠️  unknown make status\n`);
